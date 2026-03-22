075aa9452f933b8b817f5c024cd568a21a6575aa
safarovyusif <safarov.yusif@yahoo.com>
2026-03-22 19:45:32 +0000

    task

---
+++ challenge/4-delete_dnodeint/free_dlistint.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
